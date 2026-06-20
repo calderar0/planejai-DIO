d850c32e2f373ad7dc19eed091988698af22bd57
calderar0 <zer0spartagames@gmail.com>
2026-06-20 12:07:56 -0300

    Pagina de resultado montada

--- planejai/src/components/features/Simulation/Form.tsx
+++ planejai/src/components/features/Simulation/Form.tsx
@@ -1,15 +1,27 @@
-import { simulationFormSteps } from '@/data/simulations'
+import { simulationFormSteps, type SimulationFormData } from '@/data/simulations'
 import { FormStep } from './FormStep'
 import { useState } from 'react'
 import { StepProgress } from './Progress'
+import { useSimulationStorage } from '@/hooks/useSimulationStorage'
+import { useNavigate } from 'react-router-dom'
 
 export const SimulationForm = () => {
+  const {saveFormData} = useSimulationStorage()
+  const navigate = useNavigate()
   const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const [formData, setFormData] = useState<SimulationFormData>({} as SimulationFormData)
   const totalSteps = simulationFormSteps.length
   const currentStep = simulationFormSteps[currentStepIndex]
 
-  const handleNextStep = () => {
+  const handleNextStep = (value: string) => {
+    const updatedFormData = {
+      ...formData,
+      [currentStep.id]: value,
+    }
+    setFormData(updatedFormData)
     if (currentStepIndex + 1 > totalSteps - 1) {
+      saveFormData(updatedFormData)
+      void navigate('/resultado')
       return
     }
 
